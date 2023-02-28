79616cd8c68da3fd634b245ee3ccb292bb06ed4d
Pedro Neto <pedro@tuxcuiabano.com>
2023-02-28 19:26:44 -0400

    commit 1

---
+++ GaleriaDeArte/build/generated-sources/ap-source-output/ObraDeArt_.java
@@ -0,0 +1,29 @@
+import java.util.Calendar;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(ObraDeArt.class)
+public abstract class ObraDeArt_ {
+
+	public static volatile SingularAttribute<ObraDeArt, String> dimensoes;
+	public static volatile SingularAttribute<ObraDeArt, String> tipo;
+	public static volatile SingularAttribute<ObraDeArt, String> procedencia;
+	public static volatile SingularAttribute<ObraDeArt, String> titulo;
+	public static volatile SingularAttribute<ObraDeArt, String> tecnica;
+	public static volatile SingularAttribute<ObraDeArt, Integer> id;
+	public static volatile SingularAttribute<ObraDeArt, Calendar> dataObra;
+	public static volatile SingularAttribute<ObraDeArt, String> autor;
+
+	public static final String DIMENSOES = "dimensoes";
+	public static final String TIPO = "tipo";
+	public static final String PROCEDENCIA = "procedencia";
+	public static final String TITULO = "titulo";
+	public static final String TECNICA = "tecnica";
+	public static final String ID = "id";
+	public static final String DATA_OBRA = "dataObra";
+	public static final String AUTOR = "autor";
+
+}
+
---
+++ GaleriaDeArte/src/java/Testa.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tuxcuiabano
+ */
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tuxcuiabano
+ */
+public class Testa {
+    
+
+    
+public static void main(String[] args) {
+    
+EntityManagerFactory factory = Persistence.createEntityManagerFactory("obras"); 
+ 
+EntityManager em = factory.createEntityManager();
+    
+ ObraDeArt obra = new ObraDeArt();  
+ obra.setAutor("Adir Sodré");
+ obra.setTitulo("Caminho de São Francisco");
+ obra.setProcedencia("Museu da Caixa D'Agua");
+ obra.setTipo("quadro");
+ obra.setDataObra(parseData("25/11/1988"));
+ obra.setTecnica("Óleo sobre tela");
+ obra.setDimensoes("154cm x 154cm");
+ 
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+    em.persist(obra);
+    tx.commit();
+    em.close();
+    factory.close();
+ 
+  
+
+ }  
+
+private static Calendar parseData(String data){
+    try {
+        Date date = new SimpleDateFormat("dd/MM/yyyy").parse(data);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar;
+    } catch (ParseException ex) {
+      throw new IllegalArgumentException(ex);
+    }
+}
+}
